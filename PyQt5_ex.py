6aabffdcb774bc051b6b204f29217493e84791f0
rocmc <corea811@gmail.com>
2017-12-16 17:42:16 +0900

    03day

---
+++ PyQt5_ex.py
@@ -0,0 +1,10 @@
+# #coding: euc-kr
+
+import sys
+from PyQt5.Qt\idgets import *
+
+app = QApplication(sys.argv)
+label + QLabel("Hello PyQt")
+label.show()
+app.exec_()
+
--- test_03_08.py
+++ test_03_08.py
@@ -1,2 +1,14 @@
 #coding: euc-kr
 
+import tkinter
+# root = tkinter.Tk()
+a=tkinter.Tk()
+b1=tkinter.Button (a,text="안녕하세요")
+b1.pack()
+b2=tkinter.Label(a, text="NO!NO!NO!")
+b2.pack()
+b3=tkinter.Checkbutton(a, text="kkk")
+b3.pack()
+a.mainloop()
+
+
